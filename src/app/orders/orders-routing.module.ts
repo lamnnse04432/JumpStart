d1e5a077e8074542563814b1c583497e7597fd11
LamNN <lamnn1996@gmail.com>
2021-12-19 23:12:52 +0700

    sd

---
+++ src/app/orders/orders-routing.module.ts
@@ -0,0 +1,16 @@
+import {RouterModule, Routes} from '@angular/router';
+import {OrdersComponent} from './orders.component';
+import {NgModule} from '@angular/core';
+
+
+const routes: Routes = [
+  {path: '', component: OrdersComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class OrdersRoutingModule {
+  static components = [OrdersComponent];
+}
